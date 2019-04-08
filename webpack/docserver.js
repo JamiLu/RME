9b2a8f08d11c62a092942e4dac2472bbe85de1f3
Jami Lu <jami.lu@example.com>
2019-04-08 23:20:40 +0300

    docs update

--- webpack/docserver.js
+++ webpack/docserver.js
@@ -4,10 +4,10 @@ const Path = require('path');
 const app = express();
 
 
-app.use('/rme/', express.static(Path.resolve(__dirname, '../docs')));
+app.use('/', express.static(Path.resolve(__dirname, '../docs')));
 
-app.get(/^\/rme\//, (req, res) => res.sendFile(Path.resolve(__dirname, '../docs/index.html')));
+app.get(/^\w*/, (req, res) => res.sendFile(Path.resolve(__dirname, '../docs/index.html')));
 
 app.listen(3080, () => {
-    open('http://localhost:3080/rme/');
+    open('http://localhost:3080/');
 });
