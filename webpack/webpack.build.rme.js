db65a0e69fef894d9b6b81d827974e98293d4ab6
Jami Lu <lu.jami90@gmail.com>
2023-05-18 21:38:21 +0300

    enhance rme build

--- webpack/webpack.build.rme.js
+++ webpack/webpack.build.rme.js
@@ -6,29 +6,8 @@ const rmeBuildPath = Path.resolve(__dirname, '../rme-build-current/');
 const rmeBuildTarget = Path.join(rmeBuildPath, rmeFile);
 const rmeDevPath = Path.resolve(__dirname, '../src/');
 
-const ls = (path) => {
-    FileSystem.readdir(path, UTF8, (err, files) => {
-        files.forEach(file => {
-            let resolved = Path.resolve(path, file);
-            if (isDir(resolved)) {
-                ls(resolved);
-            } else if (isFile(resolved)) {
-                mergeFile(resolved, rmeBuildTarget);
-            }
-        });
-    });
-}
-
-FileSystem.writeFileSync(rmeBuildTarget, '/** RME BUILD FILE **/\n');
-ls(rmeDevPath);
-
-const mergeFile = (path, toFile) => {
-    FileSystem.readFile(path, UTF8, (err, data) => {
-        let parsedData = data.replace(/^import.*\';\n$|^export default.*\;$|^export {.*\}$/gms, '');
-        FileSystem.appendFile(toFile, parsedData, err => console.log(err ? err : 'file merged '+path));
-    });
-}
-
+const priorityFiles = ['util/util.js', 'app/valueStore.js','app/functions.js','app/manager.js'];
+let mergeJobs = [];
 
 const isFile = (path) => {
     return FileSystem.statSync(path).isFile();
@@ -37,3 +16,50 @@ const isFile = (path) => {
 const isDir = (path) => {
     return FileSystem.statSync(path).isDirectory();
 }
+
+const mergeFile = (path, toFile) => {
+    const data = FileSystem.readFileSync(path, { encoding: 'UTF-8' });
+    const parsedData = data.replace(/^import.*\';\n$|^export default.*\;$|^export {.*\}$/gms, '');
+    try {
+        FileSystem.appendFileSync(toFile, parsedData);
+        console.log('file merged '+path);
+    } catch (e) {
+        console.error(e);
+    }
+    // FileSystem.readFile(path, UTF8, (err, data) => {
+    //     let parsedData = data.replace(/^import.*\';\n$|^export default.*\;$|^export {.*\}$/gms, '');
+    //     FileSystem.appendFile(toFile, parsedData, err => console.log(err ? err : 'file merged '+path));
+    // });
+}
+
+const ls = (path) => {
+
+    const files = FileSystem.readdirSync(path);
+    files.forEach((file) => {
+        const resolved = Path.resolve(path, file);
+        if (isDir(resolved)) {
+            ls(resolved);
+        } else if (isFile(resolved)) {
+            mergeJobs.push({name: resolved, job: () => mergeFile(resolved, rmeBuildTarget)});
+        }
+    });
+}
+
+FileSystem.writeFileSync(rmeBuildTarget, '/** RME BUILD FILE **/\n');
+ls(rmeDevPath);
+// console.log('jobs', mergePromises);
+
+priorityFiles.forEach((pf, i) => {
+
+    const removed = mergeJobs.splice(mergeJobs.findIndex(job => {
+        // console.log('job', job);
+        return job.name.search(pf) > -1;
+    }), 1);
+    // console.log('removed', removed)
+    mergeJobs.splice(i, 0, removed[0]);
+
+});
+
+
+// console.log('jobs', mergePromises);
+mergeJobs.forEach(mergeJob => mergeJob.job());
