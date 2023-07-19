f4fa340f9438c66c840d427695d871225c4e77d0
Jami Lu <lu.jami90@gmail.com>
2023-07-20 00:25:38 +0300

    update packages

--- webpack/docserver.js
+++ webpack/docserver.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const open = require('opn');
 const Path = require('path');
 const app = express();
-
 
 app.use('/', express.static(Path.resolve(__dirname, '../docs')));
 
 app.get(/^\w*/, (req, res) => res.sendFile(Path.resolve(__dirname, '../docs/index.html')));
 
-app.listen(3080, () => {
-    open('http://localhost:3080/');
+app.listen(3080, async () => {
+    const open = await import('open');
+    await open.default('http://localhost:3080/');
 });
