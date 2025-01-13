4e0b1c39904302fd782752e9d8e79e8ea2ce3926
Jami Lu <lu.jami90@gmail.com>
2025-01-14 00:05:59 +0200

    fix literal class doesnt support underscores

--- src/template/template.js
+++ src/template/template.js
@@ -255,15 +255,15 @@ const RMETemplateResolver = (function() {
             }
 
             match = tag.match(/[a-z0-9]+\#[a-zA-Z0-9\-]+/); //find id
-            if (!Util.isEmpty(match))
+            if (Util.notEmpty(match))
                 resolved.setId(match.join().replace(/[a-z0-9]+\#/g, ""));
 
-            match = this.cutAttributesIfFound(tag).match(/\.[a-zA-Z-0-9\-]+/g); //find classes
-            if (!Util.isEmpty(match)) 
+            match = this.cutAttributesIfFound(tag).match(/\.[a-zA-Z-0-9_\-]+/g); //find classes
+            if (Util.notEmpty(match))
                 resolved.addClasses(match.join(" ").replace(/\./g, ""));
 
             match = tag.match(/\[[a-zA-Z0-9\= \:\(\)\#\-\_\/\.&%@!?£$+¤|;\\<\\>\\{}"]+\]/g); //find attributes
-            if (!Util.isEmpty(match))
+            if (Util.notEmpty(match))
                 resolved = Template.addAttributes(resolved, match);
 
             return resolved;
